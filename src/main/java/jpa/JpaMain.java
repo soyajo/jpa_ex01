6491d867d06cce7179c0ea7109edcc2c18586eab
josoya <thdi4564@nate.com>
2023-03-06 11:08:13 +0900

    고급매핑 - 상속관계 매핑, 공통 매핑

--- src/main/java/jpa/JpaMain.java
+++ src/main/java/jpa/JpaMain.java
@@ -371,7 +371,11 @@ public class JpaMain {
             /**
              * @MappedSuperclass
              * - 공통 매핑 정보가 필요할 때 사용 (id, name)
-             *
+             * - 상속관계 매핑 x
+             * - 엔티티 x , 테이블과 매핑 x
+             * - 부모 클래스를 상속 받는 자식 클래스에 매핑 정보만 제공
+             * - 조회, 검색 불가(em.find(BaseEntity) 불가)
+             * - 직접 생성해서 사용할 일이 없으므로 추상 클래스 권장
              */
             Member member = new Member();
             member.setName("user1");
--- src/main/java/jpa/domain/BaseEntity.java
+++ src/main/java/jpa/domain/BaseEntity.java
@@ -5,10 +5,16 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 /**
- * 공통 매핑 정보가 필요할 때 사용 (id, name)
+ * 공통 매핑
+ * - 공통 매핑 정보가 필요할 때 사용 (id, name)
+ * - 상속관계 매핑 x
+ * - 엔티티 x , 테이블과 매핑 x
+ * - 부모 클래스를 상속 받는 자식 클래스에 매핑 정보만 제공
+ * - 조회, 검색 불가(em.find(BaseEntity) 불가)
+ * - 직접 생성해서 사용할 일이 없으므로 추상 클래스 권장
  */
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
     @Column(name = "INSERT_USERNAME")
     private String createdBy;
     @Column(name = "INSERT_DATE")
